d0b4b9c6b39b92d0a2b3ef550fde6dc0b686329a
juanGalati <jcgalati14@gmail.com>
2023-07-25 15:43:36 -0300

    Resuelto: [P2] Ejercicio 1

--- Práctica 2/Ejercicio 1/ejercicio1.pas
+++ Práctica 2/Ejercicio 1/ejercicio1.pas
@@ -7,7 +7,7 @@
     La información del archivo se encuentra ordenada por código de empleado 
     y cada empleado puede aparecer más de una vez en el archivo de comisiones.
 
-    [ ] Realice un procedimiento que reciba el archivo anteriormente descripto 
+    [X] Realice un procedimiento que reciba el archivo anteriormente descripto 
         y lo compacte. En consecuencia, deberá generar un nuevo archivo en el cual,
         cada empleado aparezca una única vez con el valor total de sus comisiones.
         
@@ -16,10 +16,62 @@
 }
 
 Program ejercicio1;
-
 {$codepage utf-8}
 
 Uses crt;
 
+Const 
+  ARCHIVO_DE_COMISIONES = 'comisiones.dat';
+  ARCHIVO_DE_EMPLEADOS = 'empleados.dat';
+
+  NULO = MAXINT * -1;
+
+Type 
+  str = String[20];
+
+  t_Empleado = Record
+    codigo: integer;
+    nombre: str;
+    comision: real;
+  End;
+
+  t_Archivo = file Of t_Empleado;
+
+Procedure Leer(Var archivo: t_Archivo; Var empleado: t_Empleado);
 Begin
+  If (Not EOF(archivo))
+    Then Read(archivo, empleado)
+  Else empleado.codigo := NULO;
+End;
+
+Var 
+  maestro, detalle: t_Archivo;
+  actual, empleado: t_Empleado;
+Begin
+  Assign(detalle, ARCHIVO_DE_COMISIONES);
+  Reset(detalle);
+
+  Assign(maestro, ARCHIVO_DE_EMPLEADOS);
+  Rewrite(maestro);
+
+  Leer(detalle, empleado);
+  While (empleado.codigo <> NULO) Do
+    Begin
+      actual := empleado;
+      actual.comision := 0;
+
+      While (empleado.codigo = actual.codigo) Do
+        Begin
+          actual.comision := actual.comision + empleado.comision;
+
+          Leer(detalle, empleado);
+        End;
+
+      Write(maestro, actual);
+    End;
+
+  Close(maestro);
+  Close(detalle);
+
+  ReadLn;
 End.
