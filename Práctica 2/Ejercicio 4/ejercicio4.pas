55176a31d7bc7af1b305ceff6bdf9dd7177dd27f
juanGalati <jcgalati14@gmail.com>
2023-08-01 09:29:50 -0300

    [WiP] Práctica 2 - Ejercicio 4

---
+++ Práctica 2/Ejercicio 4/ejercicio4.pas
@@ -0,0 +1,80 @@
+
+Program ejercicio4;
+
+{$CODEPAGE UTF8}
+
+Uses 
+crt, sysutils;
+
+Const 
+  RUTA_LOGS = 'var/logs/';
+  RUTA_TEMP = 'var/temp/';
+
+  C_MAQUINAS = 5;
+
+  MININT = MAXINT * -1;
+
+Type 
+  TTemp = Record
+
+  End;
+
+  TLog = Record
+
+  End;
+
+  fo_TTemp = file Of TTemp;
+  fo_TLog = file Of TLog;
+
+  a_TTemp = Array [1..C_MAQUINAS] Of fo_TTemp;
+
+Procedure Leer(Var archivo: ; Var dato: );
+Begin
+End;
+
+Procedure Leer(Var archivo: ; Var dato: );
+Begin
+End;
+
+Procedure Leer(Var arreglo: ; Var dato: );
+Begin
+End;
+
+Procedure Escribir(Var archivo: ; Var dato: );
+Begin
+End;
+
+Procedure Escribir(Var archivo: ; Var dato: );
+Begin
+End;
+
+Procedure Escribir(Var arreglo: ; Var dato: );
+Begin
+End;
+
+Procedure Imprimir(Var archivo: ; Var dato: );
+Begin
+End;
+
+Procedure Imprimir(Var archivo: ; Var dato: );
+Begin
+End;
+
+Procedure Imprimir(Var arreglo: ; Var dato: );
+Begin
+End;
+
+Procedure Minimo(arregloArchivos: ; Var arregloRegistros: ; Var resultado: );
+Begin
+End;
+
+Procedure Actualizar(Var destino: ; Var fuente: );
+Begin
+End;
+
+Procedure Exportar(Var destino: ; Var fuente: );
+Begin
+End;
+
+Begin
+End.
